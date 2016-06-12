478902b2d121d794caf001a76bce0b6c801ac355
Steffen Köhler <Steffen Khler>
2016-06-12 14:23:02 +0200

    textBox with Button to open a video

---
+++ AVS-Script-Creator/AVS-Script-Creator/FormMain.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace AVS_Script_Creator
+{
+    partial class formMain
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.menüToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.queueToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBoxVideo = new System.Windows.Forms.TextBox();
+            this.labelVideo = new System.Windows.Forms.Label();
+            this.openFileDialogVideo = new System.Windows.Forms.OpenFileDialog();
+            this.buttonOpenVideo = new System.Windows.Forms.Button();
+            this.menuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menüToolStripMenuItem,
+            this.queueToolStripMenuItem});
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.Size = new System.Drawing.Size(1229, 24);
+            this.menuStrip.TabIndex = 1;
+            this.menuStrip.Text = "menuStrip";
+            // 
+            // menüToolStripMenuItem
+            // 
+            this.menüToolStripMenuItem.Name = "menüToolStripMenuItem";
+            this.menüToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.menüToolStripMenuItem.Text = "Menü";
+            // 
+            // queueToolStripMenuItem
+            // 
+            this.queueToolStripMenuItem.Name = "queueToolStripMenuItem";
+            this.queueToolStripMenuItem.Size = new System.Drawing.Size(54, 20);
+            this.queueToolStripMenuItem.Text = "Queue";
+            // 
+            // textBoxVideo
+            // 
+            this.textBoxVideo.Location = new System.Drawing.Point(52, 74);
+            this.textBoxVideo.Name = "textBoxVideo";
+            this.textBoxVideo.Size = new System.Drawing.Size(305, 20);
+            this.textBoxVideo.TabIndex = 3;
+            // 
+            // labelVideo
+            // 
+            this.labelVideo.AutoSize = true;
+            this.labelVideo.Location = new System.Drawing.Point(12, 77);
+            this.labelVideo.Name = "labelVideo";
+            this.labelVideo.Size = new System.Drawing.Size(37, 13);
+            this.labelVideo.TabIndex = 4;
+            this.labelVideo.Text = "Video:";
+            // 
+            // openFileDialogVideo
+            // 
+            this.openFileDialogVideo.FileName = "openFileDialogVideo";
+            // 
+            // buttonOpenVideo
+            // 
+            this.buttonOpenVideo.Location = new System.Drawing.Point(363, 72);
+            this.buttonOpenVideo.Name = "buttonOpenVideo";
+            this.buttonOpenVideo.Size = new System.Drawing.Size(26, 23);
+            this.buttonOpenVideo.TabIndex = 5;
+            this.buttonOpenVideo.Text = "...";
+            this.buttonOpenVideo.UseVisualStyleBackColor = true;
+            this.buttonOpenVideo.Click += new System.EventHandler(this.buttonOpenVideo_Click);
+            // 
+            // formMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1229, 509);
+            this.Controls.Add(this.buttonOpenVideo);
+            this.Controls.Add(this.labelVideo);
+            this.Controls.Add(this.textBoxVideo);
+            this.Controls.Add(this.menuStrip);
+            this.MainMenuStrip = this.menuStrip;
+            this.Name = "formMain";
+            this.Text = "AVS Script Creator";
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.ToolStripMenuItem menüToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem queueToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBoxVideo;
+        private System.Windows.Forms.Label labelVideo;
+        private System.Windows.Forms.OpenFileDialog openFileDialogVideo;
+        private System.Windows.Forms.Button buttonOpenVideo;
+    }
+}
+
---
+++ AVS-Script-Creator/AVS-Script-Creator/FormMain.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AVS_Script_Creator
+{
+    public partial class formMain : Form
+    {
+        public formMain()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonOpenVideo_Click(object sender, EventArgs e)
+        {
+            Stream myStream = null;
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            openFileDialog1.InitialDirectory = "c:\\";
+            openFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 2;
+            openFileDialog1.RestoreDirectory = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    if ((myStream = openFileDialog1.OpenFile()) != null)
+                    {
+                        using (myStream)
+                        {
+                            this.textBoxVideo.Text = openFileDialog1.FileName.ToString();
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                }
+            }
+
+        }
+    }
+}
